e3b6f3cb1b30b515858200a546da610e3728a9ec
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-03 10:53:45 +0700

    vite setup tailwind

---
+++ javascript-bundler/src/main.ts
@@ -0,0 +1,11 @@
+import "./style.css";
+import { setupCounter } from "./counter.ts";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1 class="font-bold">Hello World</h1>
+      <button class="bg-blue-400 p-2 rounded" id="counter" type="button"></button>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
