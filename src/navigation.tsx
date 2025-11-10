fbea4900d1a13d2baf240091e18e594f855ab319
M Haidar Hanif <git@mhaidarhanif.com>
2025-11-10 20:18:35 +0700

    fix public assets

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -2,7 +2,7 @@ export function Navigation() {
   return (
     <nav className="flex justify-between w-full px-4 pt-12 pb-4">
       <button>
-        <img src="/public/menu.svg" alt="" />
+        <img src="/menu.svg" alt="" />
       </button>
       <form method="get">
         <label htmlFor="" className="hidden">
@@ -16,7 +16,7 @@ export function Navigation() {
           className=" rounded-2xl indent-2 bg-neutral-50 px-3 py-2 text-neutral-500 w-60"
         />
       </form>
-      <img src="/public/Logo.svg" alt="logo" className="w-6" />
+      <img src="/logo.svg" alt="logo" className="w-6" />
     </nav>
   );
 }
