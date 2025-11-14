3fd163e236737b2dc20acd8a8fd6346df6bbc67d
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-14 22:37:07 +0700

    feat : install shad

--- src/components/task-tab.tsx
+++ src/components/task-tab.tsx
@@ -1,15 +1,37 @@
 import { NotesRender } from "./note-taking";
 import { Tasks } from "./todolist";
 import { Habits } from "./habit-tracking";
+import dayjs from "dayjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-function dateAndNavigation({ time }: { time: string }) {
-  return <span>{time}</span>;
-}
+const dayTime = dayjs().format("D MMMM YYYY");
 
 export function TaskTab() {
   return (
-    <div className="px-5 flex flex-col gap-3">
-      <span>{dateAndNavigation({ time: "November 10, 2025" })}</span>
+    <div className="flex flex-col gap-3 px-5">
+      <div className="flex items-center justify-between px-4">
+        <span> {dayTime} </span>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="bg- rounded-2xl px-4 py-1 font-bold text-neutral-100">
+            Add
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <div id="content-box" className="w-full">
         <div className="flex flex-col gap-2">
           <NotesRender />
