d69b4f1b1077248f8df903c85695ccba2d0eda0c
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-15 08:48:59 +0700

    fixed icon add

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,39 @@
 import { Navigation } from "./components/navigation";
 import { TaskTab } from "./components/task-tab";
 import "@fontsource-variable/inter";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Plus } from "@phosphor-icons/react";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex max-w-2xl flex-col gap-10">
       <Navigation />
       <TaskTab />
+      <DropdownMenu>
+        <DropdownMenuTrigger className="fixed right-1/4 bottom-1/6 rounded-md bg-indigo-500 p-1 text-neutral-50 hover:bg-indigo-600">
+          <Plus size={35} weight="bold" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mr-4">
+          <DropdownMenuLabel>User Control panel</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="cursor-pointer hover:bg-neutral-400">
+            Quick Notes
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer hover:bg-neutral-400">
+            Task Manager{" "}
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer hover:bg-neutral-400">
+            Add Routine
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
--- src/components/task-tab.tsx
+++ src/components/task-tab.tsx
@@ -2,14 +2,6 @@ import { NotesRender } from "./note-taking";
 import { Tasks } from "./todolist";
 import { Habits } from "./habit-tracking";
 import dayjs from "dayjs";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 const dayTime = dayjs().format("D MMMM YYYY");
 
@@ -18,24 +10,6 @@ export function TaskTab() {
     <div className="flex flex-col gap-3 px-5">
       <div className="flex items-center justify-between px-4">
         <span> {dayTime} </span>
-        <DropdownMenu>
-          <DropdownMenuTrigger className="bg- rounded-2xl bg-violet-700 px-4 py-1 font-bold text-neutral-100 hover:bg-violet-800">
-            Add +
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="mr-4">
-            <DropdownMenuLabel>User Control panel</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer hover:bg-neutral-400">
-              Quick Notes
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer hover:bg-neutral-400">
-              Task Manager{" "}
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer hover:bg-neutral-400">
-              Add Routine
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
       <div id="content-box" className="w-full">
         <div className="flex flex-col gap-2">
