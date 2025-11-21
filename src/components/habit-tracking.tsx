cadfc7f9875d223c3fa1d22a5dee4ff8a9e772a4
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-21 20:19:31 +0700

    feat: function isDone

--- src/components/habit-tracking.tsx
+++ src/components/habit-tracking.tsx
@@ -12,8 +12,17 @@ import {
 } from "@phosphor-icons/react";
 import { useState } from "react";
 import { AddHabit } from "./add-habit";
+import { cn } from "@/lib/utils";
 
-const habitData = [
+type Habit = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+type Habits = Habit[];
+
+const habitData: Habits = [
   { id: 1, title: "Study", isDone: false },
   { id: 2, title: "Workout", isDone: false },
   { id: 3, title: "Meditation", isDone: false },
@@ -57,41 +66,45 @@ export function HabitItemMenu({
 }
 
 export function HabitItem({
-  HabitId,
-  title,
-  isDone,
+  habit,
   onDelete,
+  onToogleDone,
 }: {
-  HabitId: number;
-  title: string;
-  isDone: boolean;
+  habit: Habit;
   onDelete: (id: number) => void;
+  onToogleDone: (id: number) => void;
 }) {
-  const [isDoneValue, setIsDone] = useState(isDone);
-
-  function toogleDone() {
-    setIsDone((prev) => !prev);
-  }
-
   return (
     <li
-      onClick={toogleDone}
-      className={`flex items-center justify-between rounded-xl px-4 py-3 indent-1 ${isDoneValue ? "bg-green-300 transition duration-300" : "bg-neutral-200 transition duration-300"}`}
+      onClick={() => onToogleDone(habit.id)}
+      className={cn(
+        "flex items-center justify-between rounded-xl px-4 py-3 indent-1",
+        habit.isDone
+          ? "bg-green-300 transition duration-300"
+          : "bg-neutral-200 transition duration-300",
+      )}
     >
-      <span>{title}</span>
-      <HabitItemMenu id={HabitId} onDelete={onDelete} />
+      <span>{habit.title}</span>
+      <HabitItemMenu id={habit.id} onDelete={onDelete} />
     </li>
   );
 }
 
 export function Habits() {
-  const [habit, setHabits] = useState(habitData);
+  const [habits, setHabits] = useState(habitData);
 
-  function DeleteHabit(idToDelete: number) {
-    const updatedHabit = habit.filter(
-      (habitItem) => habitItem.id !== idToDelete,
-    );
-    setHabits(updatedHabit);
+  function handleDelete(id: number) {
+    const updatedHabits = habits.filter((habitItem) => habitItem.id !== id);
+    setHabits(updatedHabits);
+  }
+
+  function handleToogleDone(id: number) {
+    const updatedHabits = habits.map((habit) => {
+      if (habit.id === id) return { ...habit, isDone: !habit.isDone };
+      else return habit;
+    });
+
+    setHabits(updatedHabits);
   }
 
   return (
@@ -99,13 +112,12 @@ export function Habits() {
       <div className="flex w-full flex-col gap-1 rounded-sm">
         <h3 className="mb-2 font-semibold">Habit Tracking</h3>
         <ul className="flex flex-col gap-2">
-          {habit.map((habit) => (
+          {habits.map((habit) => (
             <HabitItem
               key={habit.id}
-              HabitId={habit.id}
-              title={habit.title}
-              isDone={habit.isDone}
-              onDelete={DeleteHabit}
+              habit={habit}
+              onDelete={handleDelete}
+              onToogleDone={handleToogleDone}
             />
           ))}
         </ul>
