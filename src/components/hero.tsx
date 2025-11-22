2a5b9744235207aaa0933545d308533d61ad30eb
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-22 13:10:18 +0700

    feat: greeting

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,19 +1,30 @@
-const greeting = [
-  "Hi",
-  "Bonjour",
-  "Hallo",
-  "Hola",
-  "你好",
-  "안녕하세요",
-  "こんにちは",
-  "Selamat Datang",
+import { useState } from "react";
+
+type Greeting1 = {
+  id: number;
+  language: string;
+  greeting: string;
+};
+
+type greetings = Greeting1[];
+
+const greetingData: greetings = [
+  { id: 1, language: "English", greeting: `Welcome` },
+  { id: 2, language: "Arab", greeting: ` أهلاً وسهل` },
+  { id: 3, language: "Germany", greeting: `Willkommen` },
+  { id: 4, language: "Mandarin", greeting: `欢迎` },
+  { id: 5, language: "Korea", greeting: `어서 오세요` },
+  { id: 6, language: "Japanese", greeting: `いらっしゃいませ` },
+  { id: 7, language: "bahasa", greeting: `Selamat Datang` },
 ];
 
 export function Hero() {
+  const [greeting, setGreeting] = useState(greetingData);
+  const getId = Math.trunc(Math.random() * greeting.length);
   return (
     <header className="flex flex-col items-center gap-1">
       <div className="flex gap-2 text-2xl">
-        <span>{greeting[Math.trunc(Math.random() * greeting.length)]},</span>
+        <span>{greeting[getId].greeting}, </span>
         <h1 className="font-semibold">Burhanudin</h1>
       </div>
       <p className="opacity-50">
