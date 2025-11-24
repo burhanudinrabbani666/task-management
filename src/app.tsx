7b70b8ee1869c832437874ce7f6a2145d25d7d32
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-24 14:18:46 +0700

    feat: move header to layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,10 @@
 import "@fontsource-variable/inter";
 import { Dashboard } from "./components/dashboard";
-import { Top } from "./components/top";
-import { Hero } from "./components/hero";
-import { Footer } from "./components/footer";
 
 export default function App() {
   return (
     <div className="flex max-w-2xl flex-col gap-20">
-      <Top />
-      <Hero />
       <Dashboard />
-      <Footer />
     </div>
   );
 }
--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router";
+import { Hero } from "../hero";
+import { Top } from "../top";
+import { Footer } from "../footer";
 
 export function Layout() {
   return (
     <div>
-      <h1>Layout</h1>
+      <header className="mb-18 flex flex-col gap-10">
+        <Top />
+        <Hero />
+      </header>
       <Outlet />
+      <Footer />
     </div>
   );
 }
