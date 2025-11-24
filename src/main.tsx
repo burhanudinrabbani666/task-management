4db6fe85a7be0b9fdd04b6854d2922dcbbccb3f7
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-24 14:05:21 +0700

    setup layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import "@/index.css";
 import App from "@/app.tsx";
 import { About } from "@/components/routes/about.tsx";
+import { Layout } from "@/components/layouts/layout";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Route>
     </Routes>{" "}
   </BrowserRouter>,
 );
