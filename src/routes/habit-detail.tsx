7135981dc9505897c8129a7038e026112b0f9b9b
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-30 13:01:35 +0700

    feat: add icon habit detail

--- src/routes/habit-detail.tsx
+++ src/routes/habit-detail.tsx
@@ -1,6 +1,6 @@
 import { Link, useParams } from "react-router";
 import { Button } from "../components/ui/button";
-import { ArrowLeftIcon } from "@phosphor-icons/react";
+import { ArrowLeftIcon, PlusIcon, TrashIcon } from "@phosphor-icons/react";
 import { cn } from "@/lib/utils";
 import { getIcon, IntialHabitData } from "@/lib/initial-data";
 import type { Habit } from "@/modules/habit/schema";
@@ -34,8 +34,28 @@ export function HabitDetail() {
                   "bg-[url(/public/check-circle.svg)] bg-size-[100px] bg-right bg-no-repeat inset-ring-2 inset-ring-green-700 transition duration-500",
               )}
             >
-              <Icon size={32} weight="duotone" />
-              <span className="text-lg font-semibold">{habit.title}</span>{" "}
+              <div className="flex w-full justify-between">
+                <div className="flex gap-4">
+                  <Icon size={32} weight="duotone" />
+                  <span className="text-lg font-semibold">
+                    {habit.title}
+                  </span>{" "}
+                </div>
+                <div className="flex gap-2">
+                  <Button
+                    variant="ghost"
+                    className="border-0 hover:bg-amber-200"
+                  >
+                    <PlusIcon size={25} weight="bold" />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="border-0 hover:bg-amber-200"
+                  >
+                    <TrashIcon size={25} weight="duotone" />
+                  </Button>
+                </div>
+              </div>
             </div>
           );
         })()}
