42524734a07a4b863793441f78921e50e694a7b7
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-06 13:38:55 +0700

    feat: handle delete on habit-detail

--- src/routes/habit-detail.tsx
+++ src/routes/habit-detail.tsx
@@ -1,9 +1,10 @@
 import { Link, useParams } from "react-router";
-import { Button } from "../components/ui/button";
+import { Button } from "@/components/ui/button";
 import { ArrowLeftIcon, PlusIcon, TrashIcon } from "@phosphor-icons/react";
 import { cn } from "@/lib/utils";
 import { getIcon } from "@/lib/initial-data";
 import type { Habit } from "@/modules/habit/schema";
+import { useState } from "react";
 
 export function HabitDetail() {
   const params = useParams();
@@ -12,14 +13,18 @@ export function HabitDetail() {
 
   // Get habit data from local storageb
   const habitData = localStorage.getItem("habits");
-  if (!habitData) {
-    return (
-      <div>
-        <span> Habit unavaible</span>
-      </div>
+  const [habit, setHabit] = useState(habitData);
+
+  function handleDelete(id = Number(habitId)) {
+    const parsedHabits = habit ? (JSON.parse(habit) as Habit[]) : [];
+    const updatedHabits = parsedHabits.filter(
+      (habitItem: Habit) => habitItem.id !== id,
     );
+    setHabit(JSON.stringify(updatedHabits));
+    localStorage.setItem("habits", JSON.stringify(updatedHabits));
   }
-  const parsedHabit = JSON.parse(habitData) as Habit[];
+
+  const parsedHabit = habit ? (JSON.parse(habit) as Habit[]) : [];
 
   return (
     <div className="space-y-5 px-6">
@@ -31,12 +36,17 @@ export function HabitDetail() {
       <div>
         {(() => {
           const habit = parsedHabit.find((habit) => habit.id === idData);
-          if (!habit) return <div>Habit not found</div>;
+          if (!habit)
+            return (
+              <div className="flex h-36 items-center justify-center">
+                <span>Habit not found</span>
+              </div>
+            );
           const Icon = getIcon(habit.iconId);
           return (
             <div
               className={cn(
-                "flex items-center gap-4 rounded-xl bg-amber-100 px-4 py-3 indent-1 inset-ring-2 inset-ring-neutral-300",
+                "flex items-center gap-4 rounded-xl px-4 py-3 indent-1 inset-ring-2 inset-ring-neutral-300",
                 habit.isDone &&
                   "bg-[url(/public/check-circle.svg)] bg-size-[100px] bg-right bg-no-repeat inset-ring-2 inset-ring-green-700 transition duration-500",
               )}
@@ -51,13 +61,14 @@ export function HabitDetail() {
                 <div className="flex gap-2">
                   <Button
                     variant="ghost"
-                    className="border-0 hover:bg-amber-200"
+                    className="border-0 hover:bg-neutral-200"
                   >
                     <PlusIcon size={25} weight="bold" />
                   </Button>
                   <Button
                     variant="ghost"
-                    className="border-0 hover:bg-amber-200"
+                    className="border-0 hover:bg-neutral-200"
+                    onClick={() => handleDelete()}
                   >
                     <TrashIcon size={25} weight="duotone" />
                   </Button>
